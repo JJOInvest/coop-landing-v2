69320154cb4f255742537a615872475408ed742b
Akim <savvinakim@gmail.com>
2024-07-03 09:44:53 +0700

    button fix

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -22,7 +22,7 @@ export const Button = ({
         <button
             {...props}
             className={cn(
-                'flex items-center gap-3 uppercase lg:h-[60px] h-[50px] lg:px-8 text-sm lg:py-5 rounded-[10px] border-[1px] border-black transition-all duration-200',
+                'flex items-center justify-center gap-3 uppercase lg:h-[60px] h-[50px] lg:px-8 text-sm lg:py-5 rounded-[10px] border-[1px] border-black transition-all duration-200',
                 {
                     'bg-black text-white hover:bg-transparent hover:text-black':
                         variant === 'solid',
