bbf6cee3351ac0e7b5ddb84dd988734d7a4d80b7
Oleg Morozov <veryn1zee@gmail.com>
2024-07-08 20:36:44 +0700

    added news

--- src/app/invest/[id]/layout.tsx
+++ src/app/invest/[id]/layout.tsx
@@ -16,7 +16,7 @@ export default async function InvestLayout({ children }: Props) {
     return (
         <>
             <InvestHeader />
-            <div className="flex flex-col lg:flex-row bg-white my-24">
+            <div className="flex flex-col lg:flex-row bg-white my-24 gap-7">
                 <div className="lg:max-w-96">
                     <div className="overflow-hidden lg:rounded-r-xl">
                         <InvestMenu />
