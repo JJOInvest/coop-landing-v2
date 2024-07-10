d4ba4341ff11ca7110f6725ba20d2d62063d89cf
Oleg Egunkov <egunkov.oleg@gmal.com>
2024-07-10 21:54:11 +0400

    invest urls fix

---
+++ src/app/[lng]/help/[id]/loading.tsx
@@ -0,0 +1,20 @@
+import '../../invest/[id]/loading.css';
+
+export default function Loading() {
+    return (
+        <div className="flex flex-col">
+            <div className="load-wrapper">
+                <div className="activity"></div>
+            </div>
+            <div className="load-wrapper big mt-4">
+                <div className="activity"></div>
+            </div>
+            <div className="load-wrapper mt-4">
+                <div className="activity"></div>
+            </div>
+            <div className="load-wrapper big mt-4">
+                <div className="activity"></div>
+            </div>
+        </div>
+    );
+}
