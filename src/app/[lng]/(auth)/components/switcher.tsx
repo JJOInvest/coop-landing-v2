b3e1b0ee1f20e18ab5348e7c56ca06830ed78b97
Oleg Egunkov <egunkov.oleg@gmal.com>
2024-07-11 19:20:38 +0400

    fix login switcher

--- src/app/[lng]/(auth)/components/switcher.tsx
+++ src/app/[lng]/(auth)/components/switcher.tsx
@@ -5,15 +5,21 @@ import { SwitcherLink } from '@/app/[lng]/(auth)/components/switcher-link';
 
 export const Switcher = () => {
     const { t } = useTranslation();
-
     const pathname = usePathname();
+
+    const getLastPathSegment = (path: string) => {
+        const segments = path.split('/').filter(Boolean);
+        return segments[segments.length - 1];
+    };
+
+    const lastSegment = getLastPathSegment(pathname);
 
     return (
         <div className="h-16 border-grey-70 border-b-[1px] flex">
-            <SwitcherLink href="/register" isActivate={pathname === '/register'}>
+            <SwitcherLink href="/register" isActivate={lastSegment === 'register'}>
                 {t('auth.form.sign-up')}
             </SwitcherLink>
-            <SwitcherLink href="/login" isActivate={pathname === '/login'}>
+            <SwitcherLink href="/login" isActivate={lastSegment === 'login'}>
                 {t('auth.form.sign-in')}
             </SwitcherLink>
         </div>
