c03ff9d64e39f2293f321b81a1625416ec00e416
Oleg Egunkov <egunkov.oleg@gmal.com>
2024-07-16 13:12:39 +0400

    video

--- src/app/[lng]/main-page/promo-video.tsx
+++ src/app/[lng]/main-page/promo-video.tsx
@@ -1,24 +1,15 @@
 'use client';
 
+import Image from 'next/image';
+import { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 
-interface ModalState {
-    isOpen: boolean;
-}
-
-const initialState: ModalState = {
-    isOpen: false,
-};
-
-interface State {
-    data: ModalState;
-    toggle: () => void;
-}
+import CrossIcon from '@/assets/icons/cross.svg';
 
 const baseSrc =
     'https://res.cloudinary.com/jjoapp-io/video/upload/v1627474304/landing/videos/promo/en-US/promo.mp4';
 
-const videos: Record<any, string> = {
+const videos: Record<string, string> = {
     ru: 'https://res.cloudinary.com/jjoapp-io/video/upload/v1627548369/landing/videos/promo/ru/promo.mp4',
     fr: 'https://res.cloudinary.com/jjoapp-io/video/upload/v1627473240/landing/videos/promo/fr-FR/promo.mp4',
     id: 'https://res.cloudinary.com/jjoapp-io/video/upload/v1627476482/landing/videos/promo/id-ID/promo.mp4',
@@ -29,13 +20,25 @@ const videos: Record<any, string> = {
 
 export const PromoVideo = () => {
     const { i18n } = useTranslation();
-
+    const videoRef = useRef<HTMLVideoElement>(null);
     const videoSrc = videos[i18n.language] || baseSrc;
+
+    const handleClose = () => {
+        const modal = document.getElementById('promoModal') as HTMLDialogElement;
+        modal.close();
+        if (videoRef.current) {
+            videoRef.current.pause();
+            videoRef.current.currentTime = 0;
+        }
+    };
 
     return (
         <dialog id="promoModal" className="modal">
             <div className="modal-box max-w-[992px]">
-                <video autoPlay muted loop controls={false} playsInline src={videoSrc} />
+                <video ref={videoRef} autoPlay muted loop controls playsInline src={videoSrc} />
+                <button onClick={handleClose}>
+                    <Image src={CrossIcon} alt="cross" className="absolute top-2 right-2 w-4 h-4" />
+                </button>
             </div>
             <form method="dialog" className="modal-backdrop"></form>
         </dialog>
