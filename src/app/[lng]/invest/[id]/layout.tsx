253b2040d442fda83636764bb43dcd1b52cd2523
Oleg Egunkov <egunkov.oleg@gmal.com>
2024-07-16 14:21:24 +0400

    fix btn hover

--- src/app/[lng]/invest/[id]/layout.tsx
+++ src/app/[lng]/invest/[id]/layout.tsx
@@ -27,7 +27,7 @@ export default async function InvestLayout({ children, params }: Props) {
                         </div>
                         <div className="bg-black hover:bg-white rounded-r-lg">
                             <Button
-                                className="mt-2 lg:mt-4 lg:block hidden"
+                                className="hover:rounded-l-none mt-2 lg:mt-4 lg:block hidden duration-0"
                                 block
                                 as="link"
                                 href={`/${i18n.language}/register`}
