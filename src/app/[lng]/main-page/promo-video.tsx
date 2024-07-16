6518dabefd75d75fe40696e8be9fae8c35a06e5b
Oleg Egunkov <egunkov.oleg@gmal.com>
2024-07-16 18:58:46 +0400

    video fix

--- src/app/[lng]/main-page/promo-video.tsx
+++ src/app/[lng]/main-page/promo-video.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import Image from 'next/image';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
+import Modal from 'react-modal';
 
-import CrossIcon from '@/assets/icons/cross.svg';
+import CrossIcon from './assets/cross.svg';
 
 const baseSrc =
     'https://res.cloudinary.com/jjoapp-io/video/upload/v1627474304/landing/videos/promo/en-US/promo.mp4';
@@ -18,7 +19,12 @@ const videos: Record<string, string> = {
     hi: 'https://res.cloudinary.com/jjoapp-io/video/upload/v1627549845/landing/videos/promo/hi-IN/promo.mp4',
 };
 
-export const PromoVideo = () => {
+interface Props {
+    open: boolean;
+    onRequestClose: () => void;
+}
+
+export const PromoVideo = ({ open, onRequestClose }: Props) => {
     const { i18n } = useTranslation();
     const videoRef = useRef<HTMLVideoElement>(null);
     const videoSrc = videos[i18n.language] || baseSrc;
@@ -32,15 +38,61 @@ export const PromoVideo = () => {
         }
     };
 
+    const overlayStyle: React.CSSProperties = {
+        alignItems: 'center',
+        background: 'rgba(0, 0, 0, 0.8)',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        zIndex: 999,
+    };
+
+    const contentStyle: React.CSSProperties = {
+        height: 0,
+        maxWidth: 'min(960px, 100vw)',
+        outline: 'none',
+        paddingTop: 'min(540px, 56.25%)', // 16:9
+        position: 'relative',
+        width: '100%',
+        border: 'none',
+        inset: '0px',
+    };
+
+    useEffect(() => {
+        if (!open) {
+            return;
+        }
+
+        document.body.style.overflow = 'hidden';
+
+        return () => {
+            document.body.style.overflow = 'visible';
+        };
+    }, [open]);
+
     return (
-        <dialog id="promoModal" className="modal">
-            <div className="modal-box max-w-[992px]">
-                <video ref={videoRef} autoPlay muted loop controls playsInline src={videoSrc} />
-                <button onClick={handleClose}>
-                    <Image src={CrossIcon} alt="cross" className="absolute top-2 right-2 w-4 h-4" />
-                </button>
-            </div>
-            <form method="dialog" className="modal-backdrop"></form>
-        </dialog>
+        <Modal
+            ariaHideApp={false}
+            isOpen={open}
+            onRequestClose={onRequestClose}
+            shouldCloseOnEsc
+            shouldCloseOnOverlayClick
+            style={{ overlay: overlayStyle, content: contentStyle }}
+        >
+            <button
+                type="button"
+                onClick={onRequestClose}
+                className="bg-transparent border-none cursor-pointer outline-none p-0 fixed right-5 top-5 transition-opacity duration-125 ease-out"
+            >
+                <Image src={CrossIcon} alt="cross" className="lg:w-8 lg:h-8 h-4 w-4" />
+            </button>
+            <video
+                className="bg-black h-full left-0 absolute top-0 w-full"
+                controls
+                src={videoSrc}
+                autoPlay
+                playsInline
+            />
+        </Modal>
     );
 };
--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -27,7 +27,6 @@ export default function Main() {
             <ImportantKnowledge />
             <HowItWorks />
             <Welcome />
-            <PromoVideo />
         </>
     );
 }
