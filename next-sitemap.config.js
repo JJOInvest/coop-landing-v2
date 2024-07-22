5d388ecb984465a4538af7660eba7c7f32e0ee3c
Oleg Egunkov <egunkov.oleg@gmal.com>
2024-07-22 13:59:29 +0400

    KOOP-2969

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-// next-sitemap.config.js
-module.exports = {
-    siteUrl: 'https://jjo.finance', // Замените на URL вашего сайта
-    generateRobotsTxt: true, // (Опционально) Создание robots.txt файл
-    // ...другие параметры конфигурации
-};
