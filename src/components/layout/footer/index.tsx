adac22841d809968d07a4230a0175e864c681a05
Oleg Egunkov <egunkov.oleg@gmal.com>
2024-07-24 11:31:57 +0400

    copyright

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import dayjs from 'dayjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
@@ -22,6 +23,7 @@ const socialMedias = [
 
 export const Footer = () => {
     const { t, i18n } = useTranslation();
+    const year = dayjs().year();
 
     const links = [
         { href: '/', text: 'dashboard' },
@@ -108,6 +110,9 @@ export const Footer = () => {
                     {t(
                         'Disclaimer: JJO-ONE PTE. LTD. (registration code - 202304367C, registered address 10 ANSON ROAD #20-05 INTERNATIONAL PLAZA SINGAPORE (079903)) do not provide investment or assets management services of any kind or services of investment advisers. Using the Website, you confirm that you are solely responsible for all transactions made within the Website, including but not limited to the risks associated with cryptocurrency trading as activity with high-level risk (see more in our Term of Use) JJO Finance LTD (registration code -13165223, registered address 91 battersea Park Road, London, England, SW8 4DU) is a Payment Agent of JJO-ONE PTE. LTD.\nYou should consider whether you understand how cryptocurrencies and their sets work, and whether you can afford to take the high risk of losing your money while trading in cryptomarkets. Cryptocurrency is a digital representation of value that functions as a medium of exchange, a unit of account, or a store of value, but it does not have legal tender status and is not generally backed or supported by any government or central bank. Due to the fact the legality cryptocurrency holding or trading is not always clear. Cryptocurrency markets and exchanges are not regulated with the same provisions or customer protections applicable to equity, option, futures, or foreign exchange investing. Legislative and regulatory changes or actions at the state or international level may adversely affect the use, transfer, exchange, and value of cryptocurrency. User will be subject to any applicable law which may apply to the blockchain. When seeing any graphics and indicators of cryptomarkets growth, you should remember ast performance is not an indication of future results, and such markets have a high volatility level. You should seek advice from an independent and suitably licensed financial advisor and ensure that you have the risk appetite, relevant experience and knowledge before you decide to use the instruments JJO provides to the users. Before starting usage of the JJO site, please read our Terms of Use and Risk Disclosure Statement. You expressly agree that your use of the software and/or service is at your sole risk. JJO is not liable for any loss or damage arising from your participation in the cryptocurrency trading, including within the use of the JJO platform.',
                     )}
+                </p>
+                <p className="text-primary-neutral text-xs/tight opacity-40">
+                    &copy; {year} J&#39;JO. All rights reserved.
                 </p>
             </div>
         </footer>
