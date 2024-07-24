2399d066d6a6bf494419a16b04eb46a03f08fc6e
Oleg Egunkov <egunkov.oleg@gmal.com>
2024-07-24 18:18:18 +0400

    resolve conflict

--- src/app/[lng]/invest/[id]/page.tsx
+++ src/app/[lng]/invest/[id]/page.tsx
@@ -38,8 +38,11 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function Page({ params }: Props) {
     const articleId = parseInt(params.id, 10);
+    const { t, i18n } = await getServerTranslations();
 
     const article = await getArticle(params.lng, articleId);
+    const { lng } = params;
+    const canonicalUrl = `https://jjo.finance/${lng}/invest/${articleId}`;
 
     return (
         <>
@@ -62,40 +65,31 @@ export default async function Page({ params }: Props) {
                 />
                 {params.id === '33181283962009' && <News />}
                 {params.id === '33181863366425' && <InvestCalculator />}
+                {params.id === '33961919663385' && (
+                    <Link href={`/${i18n.language}/invest/33181283962009`}>
+                        <div className="flex gap-6 justify-between p-6 rounded-xl bg-invest text-white mt-10 lg:hidden">
+                            <div>{t('index_growth_savings')}</div>
+                            <Image src={Arrow} alt="arrow" />
+                        </div>
+                    </Link>
+                )}
+                {params.id === '33181283962009' && (
+                    <Link href={`/${i18n.language}/invest/33181863366425`}>
+                        <div className="flex gap-6 justify-between p-6 rounded-xl bg-invest text-white mt-10 lg:hidden">
+                            <div>{t('crypto_market_growth_potential')}</div>
+                            <Image src={Arrow} alt="arrow" />
+                        </div>
+                    </Link>
+                )}
+                {params.id === '33181863366425' && (
+                    <Link href={`/${i18n.language}/invest/33961919663385`}>
+                        <div className="flex gap-6 justify-between p-6 rounded-xl bg-invest text-white mt-10 lg:hidden">
+                            <div>{t('what_is_jjo')}</div>
+                            <Image src={Arrow} alt="arrow" />
+                        </div>
+                    </Link>
+                )}
             </div>
         </>
-        <div className="px-0 py-20 lg:p-0 lg:max-w-[870px]">
-            <h1 className="text-4xl font-medium text-black">{article.name}</h1>
-            <div
-                className="mt-6 lg:mt-10 zendesk-content"
-                dangerouslySetInnerHTML={{ __html: article.body }}
-            />
-            {params.id === '33181283962009' && <News />}
-            {params.id === '33181863366425' && <InvestCalculator />}
-            {params.id === '33961919663385' && (
-                <Link href={`/${i18n.language}/invest/33181283962009`}>
-                    <div className="flex gap-6 justify-between p-6 rounded-xl bg-invest text-white mt-10 lg:hidden">
-                        <div>{t('index_growth_savings')}</div>
-                        <Image src={Arrow} alt="arrow" />
-                    </div>
-                </Link>
-            )}
-            {params.id === '33181283962009' && (
-                <Link href={`/${i18n.language}/invest/33181863366425`}>
-                    <div className="flex gap-6 justify-between p-6 rounded-xl bg-invest text-white mt-10 lg:hidden">
-                        <div>{t('crypto_market_growth_potential')}</div>
-                        <Image src={Arrow} alt="arrow" />
-                    </div>
-                </Link>
-            )}
-            {params.id === '33181863366425' && (
-                <Link href={`/${i18n.language}/invest/33961919663385`}>
-                    <div className="flex gap-6 justify-between p-6 rounded-xl bg-invest text-white mt-10 lg:hidden">
-                        <div>{t('what_is_jjo')}</div>
-                        <Image src={Arrow} alt="arrow" />
-                    </div>
-                </Link>
-            )}
-        </div>
     );
 }
