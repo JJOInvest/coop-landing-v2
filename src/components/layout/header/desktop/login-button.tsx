677e99361d97924d1f52eefdb0e518e8319601da
Oleg Egunkov <egunkov.oleg@gmal.com>
2024-07-25 18:47:18 +0400

    login href fix

--- src/components/layout/header/desktop/login-button.tsx
+++ src/components/layout/header/desktop/login-button.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link';
 import { getServerTranslations } from '@/i18n/server';
 
 export async function LoginButton() {
-    const { t } = await getServerTranslations();
+    const { t, i18n } = await getServerTranslations();
 
     return (
         <Link
-            href="/login"
+            href={`/${i18n.language}/login`}
             className="flex items-center bg-black px-14 h-10 text-[16px]/[100%]
              text-white hover:bg-transparent hover:text-black
              transition-all duration-300 border-black border-[1px] rounded-lg"
