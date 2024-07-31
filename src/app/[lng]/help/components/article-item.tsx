bed0d6c89ad40557d9dc64887fb1094d3fa4b5e2
Oleg Egunkov <egunkov.oleg@gmal.com>
2024-07-31 16:10:26 +0400

    fix links

--- src/app/[lng]/help/components/article-item.tsx
+++ src/app/[lng]/help/components/article-item.tsx
@@ -17,33 +17,35 @@ export const ArticleItem = ({ article, children, articleIdUrl }: Props) => {
     const toggleOpened = () => setIsOpened((isOpened) => !isOpened);
 
     return (
-        <Link
-            className="flex justify-between items-center border-t-[1px] border-black/10 lg:border-0"
-            key={article.id}
-            href={`/help/${article.id}`}
-            onClick={toggleOpened}
-        >
-            <div
-                className={cn('font-light text-sm py-4 lg:py-0 hover:text-black', {
-                    'text-black font-normal': articleIdUrl === article.id.toString(),
-                    'text-primary-neutral': articleIdUrl !== article.id.toString(),
-                })}
+        <div>
+            <Link
+                className="flex justify-between items-center border-t-[1px] border-black/10 lg:border-0"
+                key={article.id}
+                href={`/help/${article.id}`}
+                onClick={toggleOpened}
             >
-                {article.name}
-                {article.id.toString() === articleIdUrl && isOpened && (
-                    <div className="lg:hidden" id="target-element">
-                        {children}
-                    </div>
-                )}
-            </div>
-            <div className="relative border-black/5 rounded-full border-[1px] h-6 min-w-6 lg:hidden self-start mt-4">
-                <span className="absolute w-2.5 h-[1px] bg-black inset-0 m-auto" />
-                <span
-                    className={cn('absolute w-2.5 h-[1px] bg-black inset-0 m-auto', {
-                        'rotate-90': article.id.toString() !== articleIdUrl,
+                <div
+                    className={cn('font-light text-sm py-4 lg:py-0 hover:text-black', {
+                        'text-black font-normal': articleIdUrl === article.id.toString(),
+                        'text-primary-neutral': articleIdUrl !== article.id.toString(),
                     })}
-                />
-            </div>
-        </Link>
+                >
+                    {article.name}
+                </div>
+                <div className="relative border-black/5 rounded-full border-[1px] h-6 min-w-6 lg:hidden self-start mt-4">
+                    <span className="absolute w-2.5 h-[1px] bg-black inset-0 m-auto" />
+                    <span
+                        className={cn('absolute w-2.5 h-[1px] bg-black inset-0 m-auto', {
+                            'rotate-90': article.id.toString() !== articleIdUrl,
+                        })}
+                    />
+                </div>
+            </Link>
+            {article.id.toString() === articleIdUrl && isOpened && (
+                <div className="lg:hidden" id="target-element">
+                    {children}
+                </div>
+            )}
+        </div>
     );
 };
